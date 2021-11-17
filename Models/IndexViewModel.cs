f73c8c5db4e39fdf0201bbe7e5e26b67853f8524
Bron Thulke <bron@angelwebdesigns.com.au>
2021-11-17 14:03:36 +1100

    Initial commit

---
+++ Models/IndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReproducingFileUploadIssue.Models
+{
+    public class IndexViewModel
+    {
+        [UIHint("ImageUpload")]
+        [Display(Name = "Select/Update an Image")]
+        public string UploadedImageData { get; set; }
+        public string UploadedImageDataFileName { get; set; }
+
+    }
+}
